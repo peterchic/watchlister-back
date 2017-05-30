0f7f6a4c633e01e5ff3fc1ae21421740d16ad1db
Usman Pervaiz <de.usman@yahoo.com>
2017-05-30 00:19:40 -0400

    Usman completed API connection

--- app/controllers/api/v1/watchlists_controller.rb
+++ app/controllers/api/v1/watchlists_controller.rb
@@ -5,6 +5,7 @@ class Api::V1::WatchlistsController < ApplicationController
   end
 
   def create
+    
     watchlist = Watchlist.create(watchlist_params)
     render json: watchlist
   end
@@ -29,6 +30,7 @@ class Api::V1::WatchlistsController < ApplicationController
   private
 
   def watchlist_params
+   
     params.require(:watchlist).permit(:name, :description)
   end
 end
