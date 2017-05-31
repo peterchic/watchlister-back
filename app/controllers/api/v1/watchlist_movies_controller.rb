c38bf0bbceda8ffc63fad72f544352c1d41e7651
curtisgreene <curtis.j.w.greene@gmail.com>
2017-05-31 09:57:13 -0400

    wed 10a

--- app/controllers/api/v1/watchlist_movies_controller.rb
+++ app/controllers/api/v1/watchlist_movies_controller.rb
@@ -1,25 +1,23 @@
 class Api::V1::WatchlistMoviesController < ApplicationController
-	
+
 	def index
 	   watchlist_movies = WatchlistMovie.all
        render json: watchlist_movies
-	end  
+	end
 
   def create
-  	
     watchlist = Watchlist.find(watchlist_movie_params[:watchlist_id])
-   	movie = watchlist.movies.create(title: watchlist_movie_params[:movie][:title], 
+   	movie = watchlist.movies.create(title: watchlist_movie_params[:movie][:title],
    							description: watchlist_movie_params[:movie][:overview],
    							poster: watchlist_movie_params[:movie][:poster_path],
    							release_date: watchlist_movie_params[:movie][:release_date]   )
-   	redirect_to action: "index"
-   	
+   	render json: watchlist
   end
 
   private
 
  def watchlist_movie_params
- 	
+
     params.require(:watchlist_movie).permit(:watchlist_id, movie: [:title, :poster_path, :overview, :release_date])
   end
 
